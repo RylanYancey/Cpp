6c65bb613ce8de011c466cc65e8627919ff452f9
Rylan Yancey <rylany37@gmail.com>
2022-05-18 11:24:07 -0400

    Create Person.h

---
+++ BinaryTreePeople/Person.h
@@ -0,0 +1,18 @@
+
+#include <string>
+
+using namespace std;
+
+class Person {
+private:
+    string name;
+    int age;
+public:
+    Person();
+    Person(string name, int age);
+    string getName();
+    int getAge();
+    void setName(string name);
+    void setAge(int age);
+    void introduce();
+};
