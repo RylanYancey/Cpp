cf254330b42ec9ce74cc66ddbdf774da1c5a365c
Rylan Yancey <rylany37@gmail.com>
2022-05-24 10:51:30 -0400

    Explaining the Problem

--- mpi_findAllPrimes/source/secondary.cpp
+++ mpi_findAllPrimes/source/secondary.cpp
@@ -5,6 +5,7 @@ Secondary::Secondary(int rank, int size) {
     this -> rank = rank;
     this -> size = size;
 
+    // clear garbage data.
     for (int i = 0; i < buf_size; i++)
         buffer[i] = 0;
 
@@ -15,16 +16,21 @@ void Secondary::task_loop() {
 
     while(true) {
 
+        // recieve data from Primary
         int data[3];
         MPI_Recv(&data, 3, MPI_INT, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
+        // exit code 0
         if (data[2] == 0)
             break;
 
+        // calculate all primes.
         find_primes(data[0], data[0] + data[1]);
 
+        // Send data back to primary. 
         MPI_Send(buffer, buf_size, MPI_INT, 0, 99, MPI_COMM_WORLD);
 
+        // clear the buffer
         for (int i = 0; i < buf_size; i++)
             buffer[i] = 0;
 
@@ -32,6 +38,7 @@ void Secondary::task_loop() {
 
 }
 
+// Verified working. 
 void Secondary::find_primes(int start, int end) {
     int index = 0;
     if (start == 0) {
